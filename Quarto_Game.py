d3afd2499109b715b559b98387412d6d9d211264
Frode <fsnpedersen@gmail.com>
2019-02-25 15:16:17 +0100

    Hello World

---
+++ Quarto_Game.py
@@ -0,0 +1,16 @@
+
+
+class Piece():
+
+    def __init__(self):
+        self.TALL = 0
+        self.SHORT = 0
+
+        self.SQUARE = 0
+        self.ROUND = 0
+
+        self.BLACK = 0
+        self.WHITE = 0
+
+        self.INDENTED = 0
+        self.FILLED = 0
---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+
+def main():
+
+    print(f'HELLO WORLD')
+
+
+if __name__ == '__main__':
+    main()
